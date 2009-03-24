0dcc159a30786df3885a7c203a3dba5c5ad8adf1
Shawn M Moore <sartak@gmail.com>
2009-03-23 22:49:27 -0400

    Initial implementation

---
+++ lib/MooseX/InstanceTracking.pm
@@ -0,0 +1,37 @@
+package MooseX::InstanceTracking;
+use Moose::Role;
+use Set::Object::Weak;
+
+has _instances => (
+    is      => 'ro',
+    isa     => 'Set::Object::Weak',
+    default => sub { Set::Object::Weak->new },
+    lazy    => 1,
+);
+
+sub instances {
+    my $self = shift;
+    @{ $self->_instances };
+}
+
+sub track_instance {
+    my $self = shift;
+    $self->_instances->insert(@_);
+}
+
+around 'construct_instance', 'clone_instance' => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $instance = $orig->($self, @_);
+    $self->track_instance($instance);
+
+    return $instance;
+};
+
+before make_immutable => sub {
+    confess "Instance tracking does not yet work with make_immutable.";
+};
+
+1;
+
